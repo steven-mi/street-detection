8a69791b044e80189af569c08571d6e9ede14de9
steven-mi <stevenmi@live.de>
2020-06-24 23:16:20 +0200

    add model download if model is not available

--- street_situation_detection/__init__.py
+++ street_situation_detection/__init__.py
@@ -7,9 +7,24 @@ to be used as a image analysing tool It can detect if a image was taken
 from a street or not.
 """
 import os
+import requests
+import zipfile
+import io
 
 import tensorflow as tf
 
+
+def download_and_extract(zip_file_url, output_folder):
+    r = requests.get(zip_file_url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(output_folder)
+
+
 this_dir, _ = os.path.split(__file__)
 model_path = os.path.join(this_dir, "model", "street-situation-network")
+model_link = "https://github.com/steven-mi/street-situation-detection/releases/download/v0.1/street-situation-network.zip"
+if not os.path.isdir(model_path):
+    os.makedirs(model_path)
+    download_and_extract(model_link, model_path)
+
 model = tf.keras.models.load_model(model_path)
